bd4349e1fc3e8b79b5d329261c125a1648de9198
John de Wasseige <johndewasseige@me.com>
2018-05-20 23:21:53 +0200

    add power cut diagram

--- code/src/reinforcement.jl
+++ code/src/reinforcement.jl
@@ -2,8 +2,10 @@ include("markov.jl")
 
 include("savecosts.jl")
 
-function qvalueiteration(cost_atk)
-    nb_time_steps = 10
+using Plots, PyPlot
+
+function qvalueiteration(cost_akt)
+    nb_time_steps = 169
     nb_stages = 18
     nb_actions = 2*3^3
     qval = zeros(nb_time_steps, nb_stages, nb_actions)
@@ -12,7 +14,8 @@ function qvalueiteration(cost_atk)
     
     alpha = 0.9
     beta = 1
-    while alpha > 0.001
+    while alpha > 0.01
+        print(alpha, "\n")
         for t = 2:nb_time_steps
 		    for k = 1:nb_stages
 		    	state = ind_to_vect(k,3,3)[1]
@@ -36,11 +39,62 @@ function qvalueiteration(cost_atk)
     end
     return opt_action
 end
-cost_akt = loadcost()
-actions = qvalueiteration(cost_akt)
 
-for t = 1:size(actions)[1]
-    print(actions[t,size(actions)[2]])
+function run_and_save_bestactions()
+    cost_akt = loadcost()
+    opt_actions = qvalueiteration(cost_akt)
+    save("./data/opt_actions.jld", "opt_actions", opt_actions)
 end
-    
 
+# run_and_save_bestactions()
+
+raw = load("./data/opt_actions.jld")
+opt_actions = raw["opt_actions"]
+
+nb_time_steps = size(opt_actions)[1]
+nb_stages = size(opt_actions)[2]
+
+state_min = 1
+power_cut = zeros(nb_time_steps)
+
+for t = 2:nb_time_steps
+    iter_state = rand(state_min:18)
+    # once we set the load OFF, we have to keep it OFF till the end
+    # if (iter_state[1] > 9)
+    #     state_min = 10
+    # end
+    iter_action = ind_to_vect(opt_actions[t,iter_state], 4, 3)
+    action_h3 = iter_action[4]
+    # print("House 3 : ", action_h3, "\n")
+    if action_h3 == 1
+        power_cut[t] = 0
+    elseif action_h3 == 2
+        power_cut[t] = 0.5
+    else 
+        power_cut[t] = 1
+    end
+end
+
+data = matread("./data/data_4nodes.mat")
+p_pred = data["pSolarMax"][:, 3]
+
+p_pred = p_pred*1000
+
+p_grid = power_cut.*p_pred
+
+fig = plot()
+
+times = [i for i=1:nb_time_steps]
+# plot!(times, p_grid,
+#    label=["Power cut"],
+#    xlabel=("Time step"),
+#    ylabel=("Power in kW"))
+# plot!(times, p_grid, marker="o", label=["Power forecast"])
+
+# histogram(p_grid, nbins = 20)
+
+bar(times, p_grid, xticks([],[]), xlabel("Time step"), ylabel("Power in kW"))
+show()
+
+# show()
+savefig(fig, "../fig.pdf")
